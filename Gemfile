21edd2d59d3824acee55008fc39b9b9f48fed765
iamkinglana <ngetichenock32@gmail.com>
2023-06-04 23:03:02 +0300

    First Commit

--- Gemfile
+++ Gemfile
@@ -49,4 +49,5 @@ group :test do
   gem "rspec-json_expectations", "~> 2.2"
 end
 
+gem 'puma'
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < Sinatra::Base
   set default_content_type: "application/json"
-  
+
   get '/bakeries' do
     bakeries = Bakery.all
     bakeries.to_json
   end
-  
+
   get '/bakeries/:id' do
     bakery = Bakery.find(params[:id])
     bakery.to_json(include: :baked_goods)
@@ -21,6 +21,24 @@ class ApplicationController < Sinatra::Base
     # see the BakedGood class for the  method definition of `.by_price`
     baked_good = BakedGood.by_price.first
     baked_good.to_json
+  end
+
+
+   post '/baked_goods' do
+    data = BakedGood.create(name: params[:name], price: params[:price], bakery_id: params[:bakery_id])
+    data.to_json
+  end
+
+  patch '/bakeries/:id' do
+    data = Bakery.find(params[:id])
+    data.update(name: params[:name])
+    data.to_json
+  end
+
+  delete '/baked_goods/:id' do
+    data = BakedGood.find(params[:id])
+    data.destroy
+    data.to_json
   end
 
 end
